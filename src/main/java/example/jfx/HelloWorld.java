13f0c126ecbaa58dc80f1d44913315918784f63e
Simon Roberts <simon@dancingcloudservices.com>
2020-08-17 14:13:25 -0600

    14:13 Day One

--- src/main/java/example/jfx/HelloWorld.java
+++ src/main/java/example/jfx/HelloWorld.java
@@ -1,6 +1,7 @@
 package example.jfx;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -25,13 +26,19 @@ public class HelloWorld extends Application {
 
         Button button = new Button("Press Me");
         button.setOnAction(e -> {
-            try {
-                Thread.sleep(6_000); // Simulates massive computation!
-            } catch (InterruptedException interruptedException) {
-                interruptedException.printStackTrace();
-            }
-            System.out.println("in event handler " + Thread.currentThread().getName());
-            text.setText("Hello JavaFX World " + ++count[0]);
+            new Thread(() -> {
+                System.out.println("Worker thread started, name is " +  Thread.currentThread().getName());
+                try {
+                    Thread.sleep(6_000); // Simulates massive computation!
+                } catch (InterruptedException interruptedException) {
+                    interruptedException.printStackTrace();
+                }
+                Runnable r = () -> {
+                    System.out.println("in event handler " + Thread.currentThread().getName());
+                    text.setText("Hello JavaFX World " + ++count[0]);
+                };
+                Platform.runLater(r);
+            }).start();
         });
 
         flow.getChildren().addAll(button, text);
--- src/main/java/example/jfx/PropertiesEx.java
+++ src/main/java/example/jfx/PropertiesEx.java
@@ -3,9 +3,12 @@ package example.jfx;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
+import javafx.util.converter.NumberStringConverter;
 
 public class PropertiesEx extends Application {
     public static void main(String[] args) {
@@ -14,14 +17,18 @@ public class PropertiesEx extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         TextFlow root = new TextFlow();
+        Circle circle = new Circle();
+        circle.setFill(Color.BROWN);
+        circle.setRadius(100);
         TextField tf = new TextField("Hello input");
         TextField tf2 = new TextField("Hello input");
         Text output = new Text("Hello output");
 
         output.textProperty().bind(tf.textProperty());
         tf2.textProperty().bindBidirectional(tf.textProperty());
+        tf.textProperty().bindBidirectional(circle.radiusProperty(), new NumberStringConverter());
 
-        root.getChildren().addAll(tf, tf2, output);
+        root.getChildren().addAll(circle, tf, tf2, output);
 
         Scene sc = new Scene(root, 600, 100);
         stage.setTitle("Binding");
